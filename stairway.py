12a3a85d38245bbd2b752e563c0cf646c2caeadd
Mohammed Hamada <112482406+MohammedCS@users.noreply.github.com>
2022-09-06 05:01:31 +0200

    Add files via upload

--- stairway.py
+++ stairway.py
@@ -8,19 +8,32 @@ window = Tk()
 window.title("Task1: Control Sonoff Switches")
 
 # Define the size of the window
-
 window.geometry("500x300")
 
-# Make the intial value for buttons turn on
+# Make the intial value for switches turn on
 switch_1_state = True
 switch_2_state = True
 
+### Define the images required for the program
+# Switches images 
 SWITCH_ON  = PhotoImage(file = "on.png")
 SWITCH_OFF = PhotoImage(file = "off.png")
-
+# Bulbs images
 BULB_ON  =  PhotoImage(file = "bulb_on.png")
 BULB_OFF =  PhotoImage(file = "bulb_off.png")
 
+##### Functions
+"""
+    Main idea is XNor Gate for stairway switching.
+
+    switch_1   | switch_2    =>  bulb
+    ___________________________________
+    On         | On          =>  On 
+    On         | Off         =>  Off
+    Off        | On          =>  Off
+    Off        | Off         =>  On 
+"""
+# function to take action when the switch 1 toggle
 def toggle_switch_1():
     global switch_1_state 
 
@@ -34,7 +47,7 @@ def toggle_switch_1():
                                     fg = "grey")
         else:
             bulb.config(image = BULB_ON)
-            bulb_state_text.config(text = "The Bulb is on",
+            bulb_state_text.config(text = "The Bulb is On",
                                     fg = "green")
     else:
         switch_1.config(image = SWITCH_ON)
@@ -46,9 +59,10 @@ def toggle_switch_1():
                                     fg = "grey")
         else:
             bulb.config(image = BULB_ON)
-            bulb_state_text.config(text = "The Bulb is on",
+            bulb_state_text.config(text = "The Bulb is On",
                                     fg = "green")
-               
+
+# function to take action when the switch 2 toggle
 def toggle_switch_2():
     global switch_2_state 
 
@@ -62,7 +76,7 @@ def toggle_switch_2():
                                     fg = "grey")
         else:
             bulb.config(image = BULB_ON)
-            bulb_state_text.config(text = "The Bulb is on",
+            bulb_state_text.config(text = "The Bulb is On",
                                     fg = "green")
     else:
         switch_2.config(image = SWITCH_ON)
@@ -74,29 +88,34 @@ def toggle_switch_2():
                                     fg = "grey")
         else:
             bulb.config(image = BULB_ON)
-            bulb_state_text.config(text = "The Bulb is on",
+            bulb_state_text.config(text = "The Bulb is On",
                                     fg = "green")
 
-switch_1 = Button(window,
-    image = SWITCH_ON,
-    bd = 0, # no border
-    command = toggle_switch_1)
-switch_1.pack(side = RIGHT)
+# Define switch_1 and its properties
+switch_1 = Button(window,       # Define the main window 
+    image = SWITCH_ON,          # Define its image
+    bd = 0,                     # No border
+    command = toggle_switch_1)  # Define the action (function)
+switch_1.pack(side = RIGHT)     # Define its place in the window
 
-switch_2 = Button(window, 
-    image = SWITCH_ON,
-    bd = 0,
-    command = toggle_switch_2)
-switch_2.pack(side = LEFT)
+# Define switch_2 and its properties
+switch_2 = Button(window,       # Define the main window
+    image = SWITCH_ON,          # Define its image
+    bd = 0,                     # No border
+    command = toggle_switch_2)  # Define the action (function)
+switch_2.pack(side = LEFT)      # Define its place in the window
 
-bulb = Label(window,
-    image = BULB_ON)
-bulb.pack(side = BOTTOM)
+# Define bulb and its properties
+bulb = Label(window,            # Define the main window
+    image = BULB_ON)            # Define its image
+bulb.pack(side = BOTTOM)        # Define its palce in the window
 
-bulb_state_text = Label(window,
-    text = "The Bulb Is On",
-    fg  = "green",
-    font = ("Helvetica", 30))
-bulb_state_text.pack(side = TOP)
+# Define the text that appear in the program
+bulb_state_text = Label(window,     # Define the main window
+    text = "The Bulb Is On",        # The text 
+    fg  = "green",                  # Foreground color
+    font = ("Helvetica", 30))       # Font and font_size
+bulb_state_text.pack(side = TOP)    # Define its place in the window  
 
+# Start the program (Tkinter)
 window.mainloop()
